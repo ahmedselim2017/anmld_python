9901c12f0ecaa1ca12c3685552387d70289e125b
Ahmed Selim Üzüm <ahmedselimuzum@gmail.com>
2025-09-05 19:40:44 +0300

    use path settings

--- src/anmld_python/main.py
+++ src/anmld_python/main.py
@@ -13,10 +13,10 @@ from anmld_python.settings import AppSettings
 
 def main(settings_path: Path):
     with open(settings_path, "rb") as settings_f:
-        app_settings = AppSettings(**tomllib.load(settings_f))
+        AS = AppSettings(**tomllib.load(settings_f))
 
     logging.basicConfig(
-        level=app_settings.logging_level,
+        level=AS.logging_level,
         format="%(asctime)s | %s(levelname)s | %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
@@ -24,30 +24,31 @@ def main(settings_path: Path):
     logging.info("Starting ANM-LD python.")
     logging.info(f"Version: {importlib.metadata.version('anmld_python')}")
 
-    app_settings.structure_init = app_settings.structure_init.absolute()
-    app_settings.structure_target = app_settings.structure_target.absolute()
-    app_settings.out_dir = app_settings.out_dir.absolute()
+    PS = AS.path_settings
+    AS.structure_init = AS.structure_init.absolute()
+    AS.structure_target = AS.structure_target.absolute()
+    AS.out_dir = AS.out_dir.absolute()
 
-    logging.debug(f"structure_init: {app_settings.structure_init}")
-    logging.debug(f"structure_target: {app_settings.structure_target}")
-    logging.debug(f"out_dir: {app_settings.out_dir}")
+    logging.debug(f"structure_init: {AS.structure_init}")
+    logging.debug(f"structure_target: {AS.structure_target}")
+    logging.debug(f"out_dir: {AS.out_dir}")
 
-    app_settings.out_dir.mkdir(parents=True)
-    logging.debug(f"Created output directory at {app_settings.out_dir}")
+    AS.out_dir.mkdir(parents=True)
+    logging.debug(f"Created output directory at {AS.out_dir}")
 
-    cmd_load = f"module load {app_settings.amber_settings.module_name} && "
+    cmd_load = f"module load {AS.amber_settings.module_name} && "
 
-    for cycle in tqdm(range(app_settings.anmld_settings.n_cycle)):
+    for cycle in tqdm(range(AS.anmld_settings.n_cycle)):
         logging.info(f"Starting cycle: {cycle}")
 
         if cycle == 0:
-            with open(app_settings.out_dir / "AMBER_min.in", "w") as AMBER_min_in_f:
+            with open(AS.out_dir / PS.amber_min_in, "w") as AMBER_min_in_f:
                 AMBER_min_in_f.write(
                     dedent(f"""\
-                    {app_settings.run_name} minimization, implicit solvent
+                    {AS.run_name} minimization, implicit solvent
                      &cntrl
                       imin = 1,
-                      maxcyc = {app_settings.amber_settings.min_step},
+                      maxcyc = {AS.amber_settings.min_step},
                       ncyc = 50,
                       ntmin = 1,
                       drms = 0.01,
@@ -57,12 +58,11 @@ def main(settings_path: Path):
                       igb = 1,
                      &end""")
                 )
-            logging.debug(f"Wrote {app_settings.out_dir / 'AMBER_min.in'}")
 
-            with open(app_settings.out_dir / "AMBER_sim.in", "w") as AMBER_sim_in_f:
+            with open(AS.out_dir / PS.amber_sim_in, "w") as AMBER_sim_in_f:
                 AMBER_sim_in_f.write(
                     dedent(f"""\
-                    {app_settings.run_name} targeted Langevin dynamics simulation
+                    {AS.run_name} targeted Langevin dynamics simulation
                      &cntrl
                       imin = 0,
                       irest = 0,
@@ -70,9 +70,9 @@ def main(settings_path: Path):
                       ntt = 3,
                       gamma_ln = 5.0,
                       ig = -1,
-                      tempi = {app_settings.amber_settings.temp},
-                      temp0 = {app_settings.amber_settings.temp},
-                      nstlim = {app_settings.amber_settings.sim_step},
+                      tempi = {AS.amber_settings.temp},
+                      temp0 = {AS.amber_settings.temp},
+                      nstlim = {AS.amber_settings.sim_step},
                       dt = 0.002,
                       ntc = 2,
                       ntf = 2,
@@ -87,66 +87,56 @@ def main(settings_path: Path):
                      &end""")
                 )
 
-            logging.debug(f"Wrote {app_settings.out_dir / 'AMBER_sim.in'}")
-
-            path_topo_init = app_settings.out_dir / "AMBER_pdb_initial.top"
-            path_coord_init = app_settings.out_dir / "AMBER_pdb_initial.coord"
-
-            path_topo_target = app_settings.out_dir / "AMBER_pdb_target.top"
-            path_coord_target = app_settings.out_dir / "AMBER_pdb_target.coord"
-
             with open(
-                app_settings.out_dir / "AMBER_tleap_initial.in", "w"
+                AS.out_dir / PS.amber_tleap_init_in, "w"
             ) as AMBER_tleap_initial_f:
                 AMBER_tleap_initial_f.write(
                     dedent(f"""\
-                    source {app_settings.amber_settings.forcefield}
-                    x=loadpdb {app_settings.structure_init}
-                    saveamberparm x {app_settings.out_dir / "AMBER_pdb_initial.top"} {app_settings.out_dir / "AMBER_pdb_initial.coord"}
-                    y=loadpdb {app_settings.structure_target}
-                    saveamberparm y {app_settings.out_dir / "AMBER_pdb_target.top"} {app_settings.out_dir / "AMBER_pdb_target.coord"}
+                    source {AS.amber_settings.forcefield}
+                    x=loadpdb {AS.structure_init}
+                    saveamberparm x {AS.out_dir / PS.amber_pdb_init_top} {AS.out_dir / PS.amber_pdb_init_coord}
+                    y=loadpdb {AS.structure_target}
+                    saveamberparm y {AS.out_dir / PS.amber_pdb_target_top} {AS.out_dir / PS.amber_pdb_target_coord}
                     quit""")
                 )
-            logging.info(f"Wrote {app_settings.out_dir / 'AMBER_tleap_initial.in'}")
 
-            cmd_tleap = f"tleap -f {app_settings.out_dir / 'AMBER_tleap_initial.in'}"
+            cmd_tleap = f"tleap -f {AS.out_dir / PS.amber_tleap_init_in}"
             subprocess.run(
                 cmd_load + cmd_tleap,
                 shell=True,
-                cwd=app_settings.out_dir,
+                cwd=AS.out_dir,
                 check=True,
             )
-            logging.debug("Ran tleap")
 
             cmd_amber_initial = dedent(f"""\
                                     $AMBERHOME/bin/pmemd.cuda -O                                    \\
-                                        -i {app_settings.out_dir / "AMBER_min.in"}                  \\
-                                        -p {app_settings.out_dir / "AMBER_pdb_initial.top"}         \\
-                                        -c {app_settings.out_dir / "AMBER_pdb_initial.coord"}       \\
-                                        -o {app_settings.out_dir / "AMBER_pdb_initial_min.out"}     \\
-                                        -x {app_settings.out_dir / "AMBER_pdb_initial_min.coord"}   \\
-                                        -r {app_settings.out_dir / "AMBER_pdb_initial_min.rst"}     \\
+                                        -i {AS.out_dir / PS.amber_min_in}               \\
+                                        -p {AS.out_dir / PS.amber_pdb_init_top}         \\
+                                        -c {AS.out_dir / PS.amber_pdb_init_coord}       \\
+                                        -o {AS.out_dir / PS.amber_pdb_init_min_out}     \\
+                                        -x {AS.out_dir / PS.amber_pdb_init_min_coord}   \\
+                                        -r {AS.out_dir / PS.amber_pdb_init_min_rst}     \\
                                         </dev/null""")
             cmd_amber_target = dedent(f"""\
                                     $AMBERHOME/bin/pmemd.cuda -O                                    \\
-                                        -i {app_settings.out_dir / "AMBER_min.in"}                  \\
-                                        -p {app_settings.out_dir / "AMBER_pdb_target.top"}          \\
-                                        -c {app_settings.out_dir / "AMBER_pdb_target.coord"}        \\
-                                        -o {app_settings.out_dir / "AMBER_pdb_target_min.out"}      \\
-                                        -x {app_settings.out_dir / "AMBER_pdb_target_min.coord"}    \\
-                                        -r {app_settings.out_dir / "AMBER_pdb_target_min.rst"}      \\
+                                        -i {AS.out_dir / PS.amber_min_in}               \\
+                                        -p {AS.out_dir / PS.amber_pdb_target_top}       \\
+                                        -c {AS.out_dir / PS.amber_pdb_target_coord}     \\
+                                        -o {AS.out_dir / PS.amber_pdb_target_min_out}   \\
+                                        -x {AS.out_dir / PS.amber_pdb_target_min_coord} \\
+                                        -r {AS.out_dir / PS.amber_pdb_target_min_rst}   \\
                                         </dev/null""")
             subprocess.run(
                 cmd_load + cmd_amber_initial,
                 shell=True,
-                cwd=app_settings.out_dir,
+                cwd=AS.out_dir,
                 check=True,
             )
             logging.debug("Ran AMBER initial")
             subprocess.run(
                 cmd_load + cmd_amber_target,
                 shell=True,
-                cwd=app_settings.out_dir,
+                cwd=AS.out_dir,
                 check=True,
             )
             logging.debug("Ran AMBER target")
@@ -154,60 +144,61 @@ def main(settings_path: Path):
             # create initial_min.rst (rewrite to be able to read by ambmsk,
             # version problem)
             with open(
-                app_settings.out_dir / "AMBER_ptraj_rewrite_initial.in", "w"
+                AS.out_dir / PS.amber_ptraj_rewrite_init_in, "w"
             ) as amber_ptraj_rewrite_initial_f:
                 amber_ptraj_rewrite_initial_f.write(
                     dedent(f"""\
-                            trajin {app_settings.out_dir / "AMBER_pdb_initial_min.rst"}
-                            trajout {app_settings.out_dir / "AMBER_initial_min.rst"} restart""")
+                            trajin {AS.out_dir / PS.amber_pdb_init_min_rst}
+                            trajout {AS.out_dir / PS.amber_pdb_rewrite_init_min_rst} restart""")
                 )
 
             cmd_rewrite = dedent(f"""cpptraj                                                \\
-                                        {app_settings.out_dir / "AMBER_pdb_initial.top"}    \\
-                                        {app_settings.out_dir / "AMBER_ptraj_rewrite_initial.in"}""")
+                                        {AS.out_dir / PS.amber_pdb_init_top}    \\
+                                        {AS.out_dir / PS.amber_ptraj_rewrite_init_in}""")
             subprocess.run(
                 cmd_load + cmd_rewrite,
                 shell=True,
-                cwd=app_settings.out_dir,
+                cwd=AS.out_dir,
                 check=True,
             )
             logging.debug("Ran rewrite")
 
             # Create AMBER_target_min_algn.rst
             with open(
-                app_settings.out_dir / "AMBER_ptraj_align_target2initial.in", "w"
+                AS.out_dir / PS.amber_ptraj_align_target2initial_in,
+                "w",
             ) as amber_ptraj_align_target2initial_f:
                 amber_ptraj_align_target2initial_f.write(
                     dedent(f"""\
-                            parm {app_settings.out_dir / "AMBER_pdb_initial.top"} [initial-top]
-                            parm {app_settings.out_dir / "AMBER_pdb_target.top"} [target-top]
-                            trajin {app_settings.out_dir / "AMBER_pdb_target_min.rst"} parm [target-top]
-                            reference {app_settings.out_dir / "AMBER_pdb_initial_min.rst"} parm [initial-top] [initial-ref]
-                            rms ref [initial-ref]  :1-{RESNUM}@CA out {app_settings.out_dir / "AMBER_rms_target_align.dat"} 
-                            trajout {app_settings.out_dir / "AMBER_target_min_algn.rst"} restart parm [target-top]""")  # TODO: RESNUM
+                            parm {AS.out_dir / PS.amber_pdb_init_top} [initial-top]
+                            parm {AS.out_dir / PS.amber_pdb_target_top} [target-top]
+                            trajin {AS.out_dir / PS.amber_pdb_target_min_rst} parm [target-top]
+                            reference {AS.out_dir / PS.amber_pdb_init_min_rst} parm [initial-top] [initial-ref]
+                            rms ref [initial-ref]  :1-{RESNUM}@CA out {AS.out_dir / PS.amber_rms_target_align_dat} 
+                            trajout {AS.out_dir / PS.amber_target_min_algn} restart parm [target-top]""")  # TODO: RESNUM
                 )
 
-            cmd_align = dedent(f"""cpptraj                                          \\
-                                    {app_settings.out_dir / "AMBER_pdb_target.top"} \\
-                                    {app_settings.out_dir / "AMBER_ptraj_align_target2initial.in"}""")
+            cmd_align = dedent(f"""cpptraj                                  \\
+                                    {AS.out_dir / PS.amber_pdb_target_top}  \\
+                                    {AS.out_dir / PS.amber_ptraj_align_target2initial_in}""")
             subprocess.run(
                 cmd_load + cmd_align,
                 shell=True,
-                cwd=app_settings.out_dir,
+                cwd=AS.out_dir,
                 check=True,
             )
             logging.debug("Ran alignment")
 
             # create initial all-atom and alpha C pdbs
             cmd_AA_init = dedent(f"""\
-                    ambmask -p {app_settings.out_dir / "AMBER_pdb_initial.top"} \\
-                        -c {app_settings.out_dir / "AMBER_initial_min.rst"}     \\
+                    ambmask -p {AS.out_dir / PS.amber_pdb_init_top} \\
+                        -c {AS.out_dir / PS.amber_pdb_rewrite_init_min_rst}     \\
                         -prnlev 1 -out pdb""")
-            with open(app_settings.out_dir / "AMBER_pdb_initial_min.pdb", "w") as out_f:
+            with open(AS.out_dir / PS.amber_pdb_initial_min_pdb, "w") as out_f:
                 subprocess.run(
                     cmd_load + cmd_AA_init,
                     shell=True,
-                    cwd=app_settings.out_dir,
+                    cwd=AS.out_dir,
                     stdout=out_f,
                     check=True,
                 )
@@ -215,39 +206,37 @@ def main(settings_path: Path):
 
             cmd_CA_init = cmd_AA_init + " -find @CA"
             with open(
-                app_settings.out_dir / "AMBER_pdb_initial_min_C.pdb", "w"
+                AS.out_dir / PS.amber_pdb_initial_min_c_pdb, "w"
             ) as out_f:
                 subprocess.run(
                     cmd_load + cmd_CA_init,
                     shell=True,
-                    cwd=app_settings.out_dir,
+                    cwd=AS.out_dir,
                     stdout=out_f,
                     check=True,
                 )
             logging.debug("Ran ambmask for init CAs")
 
             cmd_AA_target = dedent(f"""\
-                    ambmask -p {app_settings.out_dir / "AMBER_pdb_target.top"}  \\
-                        -c {app_settings.out_dir / "AMBER_target_min_algn.rst"} \\
+                    ambmask -p {AS.out_dir / PS.amber_pdb_target_top}  \\
+                        -c {AS.out_dir / PS.amber_target_min_algn} \\
                         -prnlev 1 -out pdb""")
-            with open(app_settings.out_dir / "AMBER_pdb_target_min.pdb", "w") as out_f:
+            with open(AS.out_dir / PS.amber_pdb_target_min_pdb, "w") as out_f:
                 subprocess.run(
                     cmd_load + cmd_AA_target,
                     shell=True,
-                    cwd=app_settings.out_dir,
+                    cwd=AS.out_dir,
                     stdout=out_f,
                     check=True,
                 )
             logging.debug("Ran ambmask for target AAs")
 
             cmd_CA_target = cmd_AA_target + " -find @CA"
-            with open(
-                app_settings.out_dir / "AMBER_pdb_target_min_C.pdb", "w"
-            ) as out_f:
+            with open(AS.out_dir / PS.amber_pdb_target_min_c_pdb, "w") as out_f:
                 subprocess.run(
                     cmd_load + cmd_CA_target,
                     shell=True,
-                    cwd=app_settings.out_dir,
+                    cwd=AS.out_dir,
                     stdout=out_f,
                     check=True,
                 )
--- src/anmld_python/settings.py
+++ src/anmld_python/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Literal
+from typing import Literal, Optional
 
 from pydantic import (
     Field,
@@ -11,13 +11,46 @@ from pydantic import (
 from pydantic_settings import BaseSettings
 
 
+class PathSettings(BaseSettings):
+    amber_min_in: Path = Path("AMBER_min.in")
+    amber_sim_in: Path = Path("AMBER_sim.in")
+
+    amber_pdb_init_top: Path    = Path("AMBER_pdb_initial.top")
+    amber_pdb_init_coord: Path  = Path("AMBER_pdb_initial.coord")
+
+    amber_pdb_target_top: Path      = Path("AMBER_pdb_target.top")
+    amber_pdb_target_coord: Path    = Path("AMBER_pdb_target.coord")
+
+    amber_tleap_init_in: Path       = Path("AMBER_tleap_initial.in")
+
+    amber_pdb_init_min_out: Path    = Path("AMBER_pdb_initial_min.out")
+    amber_pdb_init_min_coord: Path  = Path("AMBER_pdb_initial_min.coord")
+    amber_pdb_init_min_rst: Path    = Path("AMBER_pdb_initial_min.rst")
+
+    amber_pdb_target_min_out: Path      = Path("AMBER_pdb_target_min.out")
+    amber_pdb_target_min_coord: Path    = Path("AMBER_pdb_target_min.coord")
+    amber_pdb_target_min_rst: Path      = Path("AMBER_pdb_target_min.rst")
+
+    amber_ptraj_rewrite_init_in: Path       = Path("AMBER_ptraj_rewrite_initial.in")
+    amber_pdb_rewrite_init_min_rst: Path    = Path("AMBER_initial_min.rst")
+
+    amber_ptraj_align_target2initial_in: Path   = Path("AMBER_ptraj_align_target2initial.in")
+    amber_rms_target_align_dat: Path            = Path("AMBER_rms_target_align.dat")
+    amber_target_min_algn: Path                 = Path("AMBER_target_min_algn.rst")
+
+    amber_pdb_initial_min_pdb: Path     = Path("AMBER_pdb_initial_min.pdb")
+    amber_pdb_initial_min_c_pdb: Path   = Path("AMBER_pdb_initial_min_C.pdb")
+
+    amber_pdb_target_min_pdb: Path     = Path("AMBER_pdb_target_min.pdb")
+    amber_pdb_target_min_c_pdb: Path   = Path("AMBER_pdb_target_min_C.pdb")
+
+
 class AmberSettings(BaseSettings):
     temp: PositiveFloat = Field(310)  # TODO: float or int?
     min_step: PositiveInt = Field(500)
     sim_step: PositiveInt = Field(100)
     forcefield: str = Field("leaprc.protein.ff14SB")
     module_name: str
-
 
 
 class ANMLDSettings(BaseSettings):
@@ -35,8 +68,11 @@ class AppSettings(BaseSettings):
     structure_init: FilePath = Field(alias="initial")
     structure_target: FilePath = Field(alias="target")
     out_dir: NewPath = Field(Path("out"))
-    logging_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "INFO"
+    logging_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = (
+        "INFO"
+    )
     mode_selection: Literal["MATLAB"] = "MATLAB"
 
     anmld_settings: ANMLDSettings = Field(alias="ANMLD")
     amber_settings: AmberSettings = Field(alias="AMBER")
+    path_settings: PathSettings = Field(alias="PATHS")
