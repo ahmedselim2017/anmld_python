52c254701d32a5ab9c117c6b05b2ccb893f1bd06
Ahmed Selim Üzüm <ahmedselimuzum@gmail.com>
2025-09-07 04:57:52 +0300

    bugfixes

--- src/anmld_python/runner.py
+++ src/anmld_python/runner.py
@@ -15,22 +15,24 @@ def run_step(
     step: int,
     step_logger: loguru.Logger,
     amber_logger: loguru.Logger,
-    AS: AppSettings,
+    app_settings: AppSettings,
 ):
+    PS = app_settings.path_settings
+
     ca_init = aa_init[(aa_init.atom_name == "CA") & (aa_init.element == "C")]
     hessian_init = ANM.build_hessian(
         coords=ca_init,
         cutoff=AppSettings.anmld_settings.rcut_ANM,
-        gamma=AS.anmld_settings.gamma_ANM,
+        gamma=app_settings.anmld_settings.gamma_ANM,
     )
     step_logger.debug("Calculated the Hessian matrix")
     W_init, V_init, Vx_init, Vy_init, Vz_init = ANM.calc_modes(
         hessian=hessian_init,
-        mode_max=AS.anmld_settings.max_mode,
+        mode_max=app_settings.anmld_settings.max_mode,
     )
     step_logger.debug("Calculated ANM modes")
 
-    mode_selection = AS.mode_selection
+    mode_selection = app_settings.mode_selection
 
     step_logger.info(
         "Using {mode_selection} method to select modes",
@@ -47,12 +49,10 @@ def run_step(
                 Vy_init=Vy_init,
                 Vz_init=Vz_init,
                 step_logger=step_logger,
-                app_settings=AS,
+                app_settings=app_settings,
             )
 
-    pred_path = AS.out_dir / AS.path_settings.step_raw_pdb_format.format(
-        step=step
-    )
+    pred_path = PS.out_dir / PS.step_raw_pdb_format.format(step=step)
 
     pred_file = fastpdb.PDBFile()
     pred_file.set_structure(pred_aa)
@@ -60,16 +60,15 @@ def run_step(
     step_logger.info(f"Wrote raw step coordinates to {pred_path}")
 
     amber_tleap_step_in_path = (
-        AS.out_dir
-        / AS.path_settings.step_amber_tleap_anm_pdb_format.format(step=step)
+        PS.out_dir / PS.step_amber_tleap_anm_pdb_format.format(step=step)
     )
 
     with open(amber_tleap_step_in_path) as amber_tleap_step_in_f:
         amber_tleap_step_in_f.write(
             dedent(f"""\
-                source {AS.amber_settings.forcefield}
+                source {app_settings.amber_settings.forcefield}
                 x=loadpdb {pred_path}
-                saveamberparm x {AS.path_settings.step_amber_tleap_coord.format(step=step)}
+                saveamberparm x {app_settings.path_settings.step_amber_tleap_coord.format(step=step)}
                 quit""")
         )
     amber_logger.trace("Wrote file at {path}", path=amber_tleap_step_in_path)
